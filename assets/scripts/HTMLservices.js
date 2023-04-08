ceaf427b67548d964c4e91d2d465790764b4d4a7
Alex <alexmihailov91@gmail.com>
2023-04-08 19:55:57 +0500

    feat: adjest grid and cards height, refactor styles

--- assets/scripts/HTMLservices.js
+++ assets/scripts/HTMLservices.js
@@ -1,24 +1,21 @@
-'use strict'
+"use strict";
 
 class HTMLService {
-
-    showElement(element) {
-        element.style.display = 'block'
+  showElement(element) {
+    element.classList.remove("hidden");
+  }
+  hideElement(element) {
+    element.classList.add("hidden");
+  }
+  randomArrGenerate(max, total) {
+    let result = [];
+    for (let i = 0; i < total; i++) {
+      let newEl = Math.round(Math.random() * (max - 1) + 1);
+      while (result.includes(newEl) == true) {
+        newEl = Math.round(Math.random() * (max - 1) + 1);
+      }
+      result.push(newEl);
     }
-    hideElement(element) {
-        element.style.display = 'none'
-    }
-    randomArrGenerate(max, total) {
-        let result = [];
-        for (let i=0; i< total; i++) {
-            let newEl = Math.round(Math.random() * (max-1) + 1)
-           while (result.includes(newEl) == true )
-           {
-            newEl = Math.round(Math.random() * (max-1) + 1)
-           }
-            result.push(newEl)
-        }
-        return result
-    }
-    
+    return result;
+  }
 }
--- pages/desktop_petstory/script.js
+++ pages/desktop_petstory/script.js
@@ -1,74 +1,87 @@
 const htmlService = new HTMLService();
-const petCardsArr = document.querySelectorAll('.card')
-const petCardsNavBtn = document.querySelectorAll('.cards-btn')
-const testimonialsArr = document.querySelectorAll('.testimonials-card__wrapper')
-const navList = document.querySelector('#navList')
-const menuToggle = document.querySelector('#menu-toggle')
-const popup = document.querySelector('.popup')
-const testimonialContainer = document.querySelector('.testimonials-cards-container')
+const petCardsArr = document.querySelectorAll(".card");
+const petCardsNavBtn = document.querySelectorAll(".cards-btn");
+const testimonialsArr = document.querySelectorAll(
+  ".testimonials-card__wrapper"
+);
+const navList = document.querySelector("#navList");
+const menuToggle = document.querySelector("#menu-toggle");
+const popup = document.querySelector(".popup");
+const testimonialContainer = document.querySelector(
+  ".testimonials-cards-container"
+);
 const rangeSlider = document.querySelector('input[type="range"]');
 
 // установка max value инпута при загрузке в зависимости от ширины экрана
-changeRangeMax()
+changeRangeMax();
 
+// отображение случайных картинок при старте
+showNextPetCards();
 
 /* функционал Скролла блока Testimonials */
 function changeRangeMax() {
-    if (screen.width < 1381) {
-        rangeSlider.max = 8
-    } else rangeSlider.max = 7
+  if (screen.width < 1381) {
+    rangeSlider.max = 8;
+  } else rangeSlider.max = 7;
 }
 
-window.addEventListener('resize', function(event) {
-    changeRangeMax()
-})
+window.addEventListener("resize", function (event) {
+  changeRangeMax();
+});
 
 rangeSlider.addEventListener("input", scrollTestimonialsFunc);
-function scrollTestimonialsFunc(){
-    const testimonialCard = document.querySelector('.testimonials-card__card')
-    const cardWidth = testimonialCard.offsetWidth
-    // console.log('cardWidth :', cardWidth);
-    const gap = 30;
-    let value = rangeSlider.value;
-    let transformXvalue = (cardWidth+gap)*value
-    testimonialContainer.style.transform = `translateX(-${transformXvalue}px)`
-
-  }
+function scrollTestimonialsFunc() {
+  const testimonialCard = document.querySelector(".testimonials-card__card");
+  const cardWidth = testimonialCard.offsetWidth;
+  // console.log('cardWidth :', cardWidth);
+  const gap = 30;
+  let value = rangeSlider.value;
+  let transformXvalue = (cardWidth + gap) * value;
+  testimonialContainer.style.transform = `translateX(-${transformXvalue}px)`;
+}
 
 /* скрытие меню при клике на затемнение */
-navList.addEventListener('click', function() {
+navList.addEventListener("click", function () {
   menuToggle.checked = false;
-})
-
+});
 
 /* Функция и слушатель перелистывания карточек*/
-petCardsNavBtn.forEach(el => el.addEventListener('click', event => showNextPetCards()))
+petCardsNavBtn.forEach((el) =>
+  el.addEventListener("click", (event) => showNextPetCards())
+);
 
 function showNextPetCards() {
-    let newRandomCardsArr = htmlService.randomArrGenerate(petCardsArr.length-1,6)
-    console.log('newRandomCardsArr :', newRandomCardsArr);
-    petCardsArr.forEach(el => htmlService.hideElement(el))
-    for (let i=0; i<newRandomCardsArr.length; i++) {
-    htmlService.showElement(petCardsArr[newRandomCardsArr[i]])
-    }
+  let newRandomCardsArr = htmlService.randomArrGenerate(
+    petCardsArr.length - 1,
+    6
+  );
+  console.log("newRandomCardsArr :", newRandomCardsArr);
+  petCardsArr.forEach((el) => htmlService.hideElement(el));
+  for (let i = 0; i < newRandomCardsArr.length; i++) {
+    htmlService.showElement(petCardsArr[newRandomCardsArr[i]]);
+  }
 }
 
 /* Попап с отзывов*/
 
-testimonialsArr.forEach(el => el.addEventListener('click', function() {
-    popup.innerHTML = ''
+testimonialsArr.forEach((el) =>
+  el.addEventListener("click", function () {
+    popup.innerHTML = "";
     let popupContent = el.cloneNode(true); // клонировать сообщение
-    let closeBtn = document.createElement('button')
-    closeBtn.classList.add('popup-close-btn')
-    popupContent.style.position = 'relative'
-    popupContent.appendChild(closeBtn)
-    popup.appendChild(popupContent)
-    htmlService.showElement(popup)
+    let closeBtn = document.createElement("button");
+    closeBtn.classList.add("popup-close-btn");
+    popupContent.style.position = "relative";
+    popupContent.appendChild(closeBtn);
+    popup.appendChild(popupContent);
+    htmlService.showElement(popup);
+  })
+);
 
-   }) )
-
-   window.onclick = function(event) {
-    if (event.target == popup || event.target.classList.contains('popup-close-btn')) {
-         popup.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (
+    event.target == popup ||
+    event.target.classList.contains("popup-close-btn")
+  ) {
+    popup.style.display = "none";
+  }
+};
